e48a8a0613819d805e61c7e722c954c7414b2e65
lorenzo26 <benedict.bueno26@gmail.com>
2019-01-14 05:10:06 +0800

    new portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+var link = window.location.href.split("#")[1]
+if(link != undefined){
+	$(document).find(".current").removeClass("current");
+	$(document).find("#"+link).addClass("current");
+}
+$(document).on("click",".toggle",function(){
+	$(".wrapper aside").stop().toggle(500);
+});
+$(document).on("click",".nav_bar .drop > a", function(e){
+	event.preventDefault();
+	var page= $(this).attr("data-id");
+	$(document).find(".current").removeClass("current")
+	$(document).find("#"+page).addClass("current")
+	$(document).find(".current").attr("class","fade-page current")
+})
+
+$(document).on("click",".page-scroll", function(e){
+	event.preventDefault();
+	$(document).find(".current").removeClass("current")
+	$(document).find("#about").addClass("current")
+	$(document).find(".current").attr("class","fade-page current")
+})
